96ccab9dfa06a3d75d6aebfe36abe8bcdf658616
Adam <adam@coderdojo.org>
2016-07-01 16:10:07 +0100

    fix bad variable names

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -3,25 +3,25 @@ window.payment_amount = null;
 var handler = StripeCheckout.configure({
   key: 'pk_live_hmWhECrTqVomfEeabgcjfjS3',
   token: function(token) {
-  	$.post( "/payment.php", {
-    		stripeToken: token.id,
-    		email: token.email,
-    		amount: payment_amount
-  	});
+    $.post( "/payment.php", {
+      stripeToken: token.id,
+      email: token.email,
+      amount: payment_amount
+    });
   }
 });
 
 $(document).ready(function(){
   $('.donateButton, .donateButtonCustom').click(function(e) {
-    var y = $(this).attr('data-name')
-    var z = $(this).attr('data-description')
-    var w = 'EUR';
-    var x = $(this).attr('data-amount') || $('input[name="donation-amount"]').val() * 100;
+    var donorName = $(this).attr('data-name')
+    var donorDescription = $(this).attr('data-description')
+    var donorCurrency = 'EUR';
+    var donorAmount = $(this).attr('data-amount') || $('input[name="donation-amount"]').val() * 100;
     handler.open({
-      name: y,
-      description: z,
-      amount: x,
-      currency: w,
+      name: donorName,
+      description: donorDescription,
+      amount: donorAmount,
+      currency: donorCurrency,
     });
     payment_amount = x;
     e.preventDefault();
