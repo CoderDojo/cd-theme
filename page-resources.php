27a9cb824f5f59191877c2395def54118a1892f4
Ross O'Neill <rossoneill@Rosss-MacBook-Pro.local>
2016-08-02 16:02:44 +0100

    changes

--- page-resources.php
+++ page-resources.php
@@ -6,29 +6,29 @@ $post = new TimberPost();
 $context['post'] = $post;
 $context['broken_content'] = explode("<!--more-->", $post->content);
 
-$context['resourcesImages'] = [   ['src'=>'../wp-content/themes/cd-theme/static/img/resources/scratch.png', 'id'=>'1', 'name'=>'Scratch'],
+$context['resourcesImages'] = [   ['src'=>'../wp-content/themes/cd-theme/static/img/resources/scratch.png', 'id'=>'1', 'name'=>'Scratch','link'=>'http://kata.coderdojo.com/wiki/Scratch_Path'],
 
-                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/html.png', 'id'=>'2', 'name'=>'HTML & CSS'],
+                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/html.png', 'id'=>'2', 'name'=>'HTML & CSS' ,'link'=>'http://kata.coderdojo.com/wiki/HTML_Path'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/js.png','id'=>'3','name'=>'JavaScript'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/js.png','id'=>'3','name'=>'JavaScript', 'link'=>'http://kata.coderdojo.com/wiki/JavaScript_Path'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/python.png', 'id'=>'4', 'name'=>'Python'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/python.png', 'id'=>'4', 'name'=>'Python' , 'link'=>'http://kata.coderdojo.com/wiki/Python_Path'],
 
-                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/ruby.png', 'id'=>'5', 'name'=>'Ruby'],
+                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/ruby.png', 'id'=>'5', 'name'=>'Ruby', 'link'=>'http://kata.coderdojo.com/wiki/Ruby_Path'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/php.png','id'=>'6','name'=>'PHP'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/php.png','id'=>'6','name'=>'PHP', 'link'=>'http://kata.coderdojo.com/wiki/PHP_Path'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/raspberrypi.png', 'id'=>'7', 'name'=>'Raspberry Pi'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/raspberrypi.png', 'id'=>'7', 'name'=>'Raspberry Pi', 'link'=>'http://kata.coderdojo.com/wiki/Raspberry_Pi'],
 
-                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/arduino.png', 'id'=>'8','name'=>'Arduino'],
+                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/arduino.png', 'id'=>'8','name'=>'Arduino', 'link'=>'http://kata.coderdojo.com/wiki/Arduino'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/mobile.png','id'=>'9','name'=>'Mobile'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/mobile.png','id'=>'9','name'=>'Mobile', 'link'=>'http://www.coderdojo.jp/'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/champion.png', 'id'=>'10', 'name'=>'Resources For Champions'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/champion.png', 'id'=>'10', 'name'=>'Resources For Champions', 'link'=>'http://kata.coderdojo.com/wiki/Organising_a_Dojo'],
 
-                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/mentors.png', 'id'=>'11','name'=>'Resources For Mentors'],
+                        [ 'src'=>'../wp-content/themes/cd-theme/static/img/resources/mentors.png', 'id'=>'11','name'=>'Resources For Mentors', 'link'=>'http://kata.coderdojo.com/wiki/Mentors_and_Volunteers'],
 
-                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/parent.png','id'=>'12','name'=>'Resources For Parents'],
+                        ['src'=>'../wp-content/themes/cd-theme/static/img/resources/parent.png','id'=>'12','name'=>'Resources For Parents', 'link'=>'http://kata.coderdojo.com/wiki/Ninja_Zone'],
                       ];
 
 $context['Resources'] = "Resources";
