65294019c1d29baaa548e483e93c8c55bf4bbabb
Daniel Brierton <daniel@coderdojo.org>
2016-11-09 15:24:01 +0000

    Add .fitvids-responsive-iframe class for making iframes responsive

--- static/site.js
+++ static/site.js
@@ -1,5 +1,5 @@
 jQuery( document ).ready( function( $ ) {
 
   // Your JavaScript goes here
-  jQuery('#content').fitVids();
+  jQuery('#content').fitVids({customSelector: ".fitvids-responsive-iframe"});
 });
