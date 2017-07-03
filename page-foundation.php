6176d6b1aca6da4091732d88cfb556e85cfe7966
Ben Skerritt <ben.skerritt77@gmail.com>
2017-07-03 09:40:02 +0100

    deleted and updated files

--- page-foundation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$context = Timber::get_context();
-$post = new TimberPost();
-$context['post'] = $post;
-$context['broken_content'] = explode("<!--more-->", $post->content);
-
-Timber::render('page-foundation.twig', $context);
