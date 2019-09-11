e963cf80df4fe39de72f29852d22ec306b65ac24
Greg Annandale <greg@raspberrypi.org>
2019-09-11 14:27:55 +0100

    Redirect when POSTing payment

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,12 +1,28 @@
 window.payment_amount = null;
 
+$.extend({
+  redirectPost: function(location, args) {
+    var form = $('<form></form>');
+    form.attr("method", "post");
+    form.attr("action", location);
+
+    $.each( args, function( key, value ) {
+      var field = $('<input></input>');
+      field.attr("type", "hidden");
+      field.attr("name", key);
+      field.attr("value", value);
+      form.append(field);
+    });
+    
+    $(form).appendTo('body').submit();
+  }
+});
+
 $(document).ready(function(){
   $('.donateButton, .donateButtonCustom').click(function(e) {
     e.preventDefault();
     var donorAmount = $(this).attr('data-amount') || $('input[name="donation-amount"]').val() * 100;
     payment_amount = donorAmount;
-    $.post( "/payment.php", {
-      amount: payment_amount
-    });
+    $.redirectPost("/payment.php", {amount: payment_amount});
   });
 });
