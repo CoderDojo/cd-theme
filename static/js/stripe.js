c01e98fc478bc7ad89f2a3f2ee280455f19b604c
Greg Annandale <greg@raspberrypi.org>
2019-09-11 14:00:59 +0100

    Remove Stripe-rendered checkout form

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,29 +1,12 @@
 window.payment_amount = null;
 
-var handler = StripeCheckout.configure({
-  key: 'pk_live_hmWhECrTqVomfEeabgcjfjS3',
-  token: function(token) {
-    $.post( "/payment.php", {
-      stripeToken: token.id,
-      email: token.email,
-      amount: payment_amount
-    });
-  }
-});
-
 $(document).ready(function(){
   $('.donateButton, .donateButtonCustom').click(function(e) {
-    var donorName = $(this).attr('data-name')
-    var donorDescription = $(this).attr('data-description')
-    var donorCurrency = 'EUR';
-    var donorAmount = $(this).attr('data-amount') || $('input[name="donation-amount"]').val() * 100;
-    handler.open({
-      name: donorName,
-      description: donorDescription,
-      amount: donorAmount,
-      currency: donorCurrency,
-    });
-    payment_amount = donorAmount;
     e.preventDefault();
+    var donorAmount = $(this).attr('data-amount') || $('input[name="donation-amount"]').val() * 100;
+    payment_amount = donorAmount;
+    $.post( "/payment.php", {
+      amount: payment_amount
+    });
   });
 });
